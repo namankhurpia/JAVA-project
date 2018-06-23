1f290968f98961eef11a02ee1306e2f9044c502f
namankhurpia <naman.khurpia2016@vitstudent.ac.in>
2018-06-23 12:11:45 +0530

    hashmap
    hashmap

---
+++ using hashmap/hashmap.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashmap;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Naman Khurpia
+ */
+public class hashmap {
+    public static void main(String[] args) {
+        
+        HashMap<String,Integer> hm= new HashMap<>();
+        hm.put("naman",1234);
+        hm.put("naman2",1256);
+        System.out.println(hm);
+        
+        
+    }
+    
+}
